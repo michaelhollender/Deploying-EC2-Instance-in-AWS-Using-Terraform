c05174ba9088263437817c802676e9e1e5a894e3
mastersplinter2 <111327102+michaelhollender@users.noreply.github.com>
2023-11-17 01:58:26 -0500

    Changed httpd to apache2 install on debian based

--- install-apache.sh
+++ install-apache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dnf update -y
-dnf install -y httpd
-systemctl start httpd
-systemctl enable httpd
+apt update -y
+apt install -y apache2
+systemctl start apache2
+systemctl enable apache2
 echo "<html><body><h1>WEB Tier EC2 Instance Deployed</h1></body></html>" > /var/www/html/index.html
